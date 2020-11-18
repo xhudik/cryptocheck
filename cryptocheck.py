d9bc0930bd386d415f632f6d1b73b46903cd016c
Tomas Hudik <Tomas.Hudik@firma.seznam.cz>
2020-11-18 22:34:32 +0100

    improved

--- cryptocheck.py
+++ cryptocheck.py
@@ -1,24 +1,43 @@
+#!/usr/bin/env python 
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import pandas as pd
-import numpy as np
+from dominate import document
+from dominate.tags import *
+#import pandas as pd
+#import numpy as np
 import re
+from time import gmtime, strftime
 import time
 
+
+#download gecko driver (from here: https://github.com/mozilla/geckodriver/releases/tag/v0.28.0)
 
 options = webdriver.FirefoxOptions()
 #options.add_argument('--ignore-certificate-errors')
 #options.add_argument("--test-type")
-driver = webdriver.Firefox(executable_path='/home/tomas/now/cryptocheck/geckodriver', options=options)
+options.headless=True
+driver = webdriver.Firefox(executable_path='./geckodriver', options=options)
 
 #accept cookies
 driver.get("https://www.tradingview.com/symbols/BTCEUR/?exchange=BINANCE")
-driver.find_element_by_xpath("//div/div/article/div[2]/div/button").click()
+try:
+	driver.find_element_by_xpath("//div/div/article/div[2]/div/button").click()
+except Exception as e:
+	print(f"No cookies info. {e}")
 
 def getGraph(TVurl):
 	driver.get(TVurl)
-	button3m  = driver.find_element_by_xpath("//div[1]/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div[4]")
-	button3m.click()
+	print(TVurl)
+	time.sleep(2)
+	try:
+		button3m  = driver.find_element_by_xpath("//div[1]/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div[4]")
+		print(button3m.is_enabled(),button3m.is_displayed())
+		button3m.click()
+	except Exception as e:
+		print(e)
+	#button3m  = driver.find_element_by_xpath("//div[1]/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div[4]")
+	#button3m.click()
 	graph  = driver.find_element_by_xpath("//div[5]/div/div/div/div/div/div[1]/div")
 	return graph
 
@@ -35,6 +54,18 @@ bnbeur.screenshot("screenshotBNBEUR.png")
 bnbbtc = getGraph("https://www.tradingview.com/symbols/BNBBTC/?exchange=BINANCE")
 bnbbtc.screenshot("screenshotBNBBTC.png")
 
+driver.quit()
 
 
+doc = document(title='BTC-TrView')
+with doc:
+	h1(f"TradingView Prices, {strftime('%Y-%m-%d %H:%M', gmtime())}")
+	a(img(src='screenshotBTCEUR.png'), target="_blank", href="https://www.tradingview.com/symbols/BTCEUR/?exchange=BINANCE") 
+	hr()
+	a(img(src='screenshotBNBEUR.png'), target="_blank", href="https://www.tradingview.com/symbols/BNBEUR/?exchange=BINANCE") 
+	hr()
+	a(img(src='screenshotBNBBTC.png'), target="_blank", href="https://www.tradingview.com/symbols/BNBBTC/?exchange=BINANCE") 
 
+page = open('stocks.html','w')
+page.write(doc.render(xhtml=True))
+page.close()
